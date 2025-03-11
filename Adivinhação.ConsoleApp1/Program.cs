449b4c00595bca433d10c1da0091328932dec394
gabrielvsantoss <gabrielvs3131@gmail.com>
2025-03-11 16:33:02 -0300

    Add project files.

---
+++ Adivinhação.ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Adivinhação.ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
